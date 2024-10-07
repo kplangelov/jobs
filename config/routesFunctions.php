e4e24be919100a64d61017f513fb0a2ebdd96dc5
kplangelov <kplangelov@gmail.com>
2024-10-08 02:18:48 +0300

    Update routesFunctions.php
    SetRoute now takes only one argument full path to file.

--- config/routesFunctions.php
+++ config/routesFunctions.php
@@ -1,40 +1,31 @@
-<?php 
+<?php
 
 
-function setRoute($phpFileName, $path = 'PHP')
+function setRoute($file)
 {
-    //we check if dir exist, and then if file exist - include.
-    
-    if(!is_dir($path))
-    {
-        echo 'Unexisting directory.';
-        return;
-    }
 
-    if(file_exists($path.DIRECTORY_SEPARATOR.$phpFileName))
-    {
+    $fullpath = $file;
+    if (file_exists($fullpath)) {
         //echo $path.DIRECTORY_SEPARATOR.$phpFileName;
-        require $path.DIRECTORY_SEPARATOR.$phpFileName;
-    }
-    else {
-        echo 'The directory is valid, but the file is not exist.';
+        require $fullpath;
+    } else {
+        echo 'File is not exist.';
     }
 }
 function getRoute($pageName)
 {
-    
-    return "index.php?page=".htmlspecialchars($pageName);
+
+    return "index.php?page=" . htmlspecialchars($pageName);
 
 }
 
 function includeForm($formName)
 {
-    $dir = 'View/HTML/Forms/'.$formName;
+    $dir = 'View/HTML/Forms/' . $formName;
 
-    if(file_exists($dir)) {
+    if (file_exists($dir)) {
         require $dir;
-    }
-    else {
-        echo 'This form not exist -> '.$dir;
+    } else {
+        echo 'This form not exist -> ' . $dir;
     }
 }
