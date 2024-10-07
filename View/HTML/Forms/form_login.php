2d8981afce6d33cf23d28a7bb783dcdc7aa6a706
kplangelov <kplangelov@gmail.com>
2024-10-07 23:30:40 +0300

    Create form_login.php
    Form for account login

---
+++ View/HTML/Forms/form_login.php
@@ -0,0 +1,10 @@
+<div class="container mt-5">
+    <h2 class="h3">Account login</h2>
+    <form method="POST" action="">
+        <label for="mail" class="form-label">Account email adress</label>
+        <input class="form-control bg-dark text-white" type="text" name="mail" id="mail" placeholder="Email adress" require>
+        <label for="pass" class="form-label">Account password</label>
+        <input class="form-control bg-dark text-white mt-2" type="text" name="pass" id="pass" placeholder="Password" require>
+        <input class="btn btn-primary mt-3" type="submit" value="Login">
+    </form>
+</div>
