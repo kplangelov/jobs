c770f66ac4b269e0a976e5632cb7e71d67ed0042
kplangelov <kplangelov@gmail.com>
2024-10-08 02:11:53 +0300

    Update index.php
    Updated routing in index, clearer code. AccountController and routing functions going to be updated now too.

--- index.php
+++ index.php
@@ -6,8 +6,10 @@ require 'View/HTML/Templates/header.php';
     Access explanation
     if access is 0, EVERYONE can open this page.
     if access is 1, only guests can open this page.
-    if access is 2, only registered users can open this page.
-    if access is 3, only admin can open this page.
+    if access is 2, is for accounts. (users and companies, admins)
+    if access is 3, only users and admins
+    if access is 4, only companies and admins
+    if access is 5, only admins.
 */
 
 $routes = [
@@ -24,51 +26,68 @@ $routes = [
         'url' => 'registration.php'
     ],
 
-    'my_jobs' => [
-        'access' => '2',
-        'url' => 'my_jobs.php'
-    ],
-
-    'view_job' => [
-        'access' => '2',
-        'url' => 'job.php'
-    ],
-
-    'create_job' => [
-        'access' => '2',
-        'url' => 'job_add.php'
-    ],
-
-    'deleted_jobs' => [
-        'access' => '2',
-        'url' => 'deleted_jobs.php'
-    ],
-
     'profile' => [
-        'access' => '2',
+        'access' => '2', //accounts
         'url' => 'profile.php'
     ],
 
     'settings' => [
-        'access' => '2',
+        'access' => '2', //accounts
         'url' => 'settings.php'
     ],
 
-    'update_job_status' => [
-        'access' => '2',
-        'url' => 'update_job_status.php'
-    ],
-
-    'logout' => [
-        'access' => '2',
-        'url' => 'logout.php'
-    ],
-
-    'admin_all_jobs' => [
-        'access' => '3',
+    'view_job' => [
+        'access' => '2', //accounts
         'url' => 'job.php'
     ],
 
+    'logout' => [
+        'access' => '2', //accounts
+        'url' => 'logout.php'
+    ],
+
+    'my_jobs' => [
+        'access' => '4', //companies
+        'url' => 'my_jobs.php'
+    ],
+
+    'create_job' => [
+        'access' => '4', //companies
+        'url' => 'job_add.php'
+    ],
+
+    'deleted_jobs' => [ // companies
+        'access' => '4',
+        'url' => 'deleted_jobs.php'
+    ],
+
+    'update_job_status' => [
+        'access' => '4', // companies
+        'url' => 'update_job_status.php'
+    ],
+
+    'upload_company_logo' => [ //companies
+        'access' => '3',
+        'url' => 'upload_company_logo.php'
+    ],
+
+
+    'admin_all_jobs' => [ //admins
+        'access' => '5',
+        'url' => 'job.php'
+    ],
+
+];
+
+$global_route_path = 'PHP';
+
+$route_paths = [
+    '0' => 'everyone',
+    '1' => 'guests',
+    '2' => 'accounts',
+    '3' => 'users',
+    '4' => 'companies',
+    '5' => 'admins'
 ];
 
 $get_page = htmlspecialchars(trim($_GET['page']));
@@ -78,54 +97,41 @@ if (!$get_page) {
 }
 
 if (!array_key_exists($get_page, $routes)) {
-    echo 'This page dont exist.';
+    echo '<div class="container alert alert-danger mt-3" role="alert">This page doesn\'t exist!</div>';
     exit();
 }
 
-$userCntrl = new UserController();
+$accCntrl = new AccountController();
 
-$logged_user = $userCntrl->isUserLoggedIn();
+$logged_acc = $accCntrl->isAccLoggedIn();
 
-$route_path = 'PHP';
-$route_paths = [
-    '0' => 'everyone',
-    '1' => 'guests',
-    '2' => 'logged',
-    '3' => 'admins'
-];
+foreach ($routes as $page => $pageData) {
 
-foreach ($routes as $page => $pageInfo) {
+    if ($get_page === $page) {
 
-    if ($get_page === $page)
-        if ($pageInfo['access'] === '0') {
-            // PAGES FOR EVERYONE
-            setRoute($pageInfo['url'], $route_path . DIRECTORY_SEPARATOR . $route_paths[$pageInfo['access']]);
+        $pageAccess = $pageData['access'];
+        $pageURL = $pageData['url'];
+        $includePath = $global_route_path . DIRECTORY_SEPARATOR . $route_paths[$pageAccess] . DIRECTORY_SEPARATOR . $pageURL;
+        $setInclude = false;
 
-        } else if ($pageInfo['access'] === '1') {
-            //PAGES FOR GUESTS AND ADMIN
-            if (!$logged_user || $logged_user['role'] == '3') {
-                setRoute($pageInfo['url'], $route_path . DIRECTORY_SEPARATOR . $route_paths[$pageInfo['access']]);
-            } else {
-                echo 'You are already logged.';
-            }
-
-        } else if ($pageInfo['access'] === '2') {
-            //PAGES FOR USERS AND ADMIN
-            if (!empty($logged_user)) {
-                setRoute($pageInfo['url'], $route_path . DIRECTORY_SEPARATOR . $route_paths[$pageInfo['access']]);
-            } else {
-                echo 'You are not logged.';
-                //require $route_path . DIRECTORY_SEPARATOR . 'error_page.php';
-            }
-        } else if ($pageInfo['access'] === '3') {
-            //PAGES FOR ADMIN ONLY.
-            if ($logged_user && $logged_user['role'] == '3') {
-                echo 'THIS IS AN ADMIN PAGE!<hr>';
-                setRoute($pageInfo['url'], $route_path . DIRECTORY_SEPARATOR . $route_paths[$pageInfo['access']]);
-            } else {
-                echo 'You are not authorized to view this page.';
-            }
-        } else {
-            echo 'You have an issue with your access value in index.php main route. Route: ' . $page;
+        if ($pageAccess == '0') {//EVERYONE
+            $setInclude = true;
+        } else if ($pageAccess == '1' && !$logged_acc) { //GUESTS ONLY
+            $setInclude = true;
+        } else if ($pageAccess == '2' && $logged_acc) { // Accounts [ USERS , COMPANIES, ADMINS ]
+            $setInclude = true;
+        } else if ($pageAccess == '3' && $logged_acc && ($logged_acc['role'] == '3' || $logged_acc['role'] == '5') ) { 
+            $setInclude = true;                                              //USERS AND ADMINS ONLY
+        } else if ($pageAccess == '4' && $logged_acc && ($logged_acc['role'] == '4' || $logged_acc['role'] == '5') ) {  
+            $setInclude = true;                                              //COMPANIES AND ADMINS ONLY
+        } else if ($pageAccess == '5' && $logged_acc && $logged_acc['role'] == '5') { //ADMINS ONLY
+            $setInclude = true;
         }
+
+        if ($setInclude) {
+            setRoute($includePath);
+        } else {
+            echo '<div class="container alert alert-danger mt-3" role="alert">You are not authorized to open this page.</div>';
+        }
+    }
 }
