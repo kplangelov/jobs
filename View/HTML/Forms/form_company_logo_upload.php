217467f667b535831a963a9a515a5341b1336833
kplangelov <kplangelov@gmail.com>
2024-10-09 03:25:42 +0300

    Update form_company_logo_upload.php

--- View/HTML/Forms/form_company_logo_upload.php
+++ View/HTML/Forms/form_company_logo_upload.php
@@ -1,7 +1,9 @@
-<form enctype="multipart/form-data" action="" method="POST">
-    <input type="hidden" name="MAX_FILE_SIZE" value="30000" />
-    <input name="company_logo" type="file" />
-    <input type="submit" class="" value="Upload">
-    <input type="hidden" name="is_upload_clicked" value="1" class="">
-
-</form>
+<div class="container mt-3">
+    <h2 class="h3">Add new company logo</h2>
+    <form enctype="multipart/form-data" action="" method="POST">
+        <input type="hidden" name="MAX_FILE_SIZE" value="2097152" />
+        <input name="company_logo" type="file" />
+        <input type="submit" class="" value="Upload">
+        <input type="hidden" name="is_upload_clicked" value="1" class="">
+    </form>
+</div>
